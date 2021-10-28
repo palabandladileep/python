b2e63063062805e95a28e47859af7287e91f92c5
Palabandla.Dileep <palabandladileep98@gmail.com>
2021-10-28 20:59:37 +0530

    Second day exercises

---
+++ login.py
@@ -0,0 +1,5 @@
+from service.validate_user import validateUser
+u = validateUser("manju", "dileep")
+
+print(u)
+
---
+++ validate_user.py
@@ -0,0 +1,5 @@
+def validateUser(user,password):
+    if(user == "dileep"):
+        return True
+    else:
+        return False
