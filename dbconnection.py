09afea25ca8cb30280e56f8546487db1912354c4
palabandladileep <92458424+palabandladileep@users.noreply.github.com>
2021-11-09 15:23:31 +0530

    Delete dbconnection.py

--- dbconnection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def getdbconnection():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Dileep@369874",
-        database="casestudy"
-    )
-    return mydb
